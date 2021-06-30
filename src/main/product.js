7625f0ca71594fc0d643593f4cdabfb8797d68a8
kwngsoo Lee <kwngsoolee@kwngsooui-MacBookPro.local>
2021-06-30 19:55:25 +0900

    react-grab-market

--- src/main/product.js
+++ src/main/product.js
@@ -3,6 +3,6 @@ import { useParams } from "react-router-dom";
 function ProductPage() {
   const { id } = useParams();
   console.log(id);
-  return <p>{id}제품상세페이지</p>;
+  return <p>{id}제품상세페이지 입니다.</p>;
 }
 export default ProductPage;
